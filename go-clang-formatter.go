d02b6139f54c46a2fd635885e8fc65d7597daf91
egawa <Tatsuya@TEgawa-no-MacBook-Air.local>
2015-02-25 09:10:04 +0900

    add UUID

--- go-clang-formatter.go
+++ go-clang-formatter.go
@@ -1,17 +1,15 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
-	//"encoding/json"
-	"encoding/base64"
 )
-
-var format_style = "LLVM"
 
 func check(e error) {
 	if e != nil {
@@ -19,12 +17,18 @@ func check(e error) {
 	}
 }
 
-func set_style(s string){
-	format_style = s
+type reqStyle struct {
+	Style string `json:"style"`
+	UUID  string `json:"uuid"`
+}
+
+type reqFormat struct {
+	Code []byte `json:"code"`
+	UUID string `json:"uuid"`
 }
 
 func get_style(s string) string {
-	switch s{
+	switch s {
 	case "LLVM":
 		return "-style=LLVM"
 	case "Google":
@@ -45,33 +49,52 @@ func style(w http.ResponseWriter, r *http.Request) {
 
 	body, err := ioutil.ReadAll(r.Body)
 	check(err)
+	fmt.Println(string(body))
 
-	set_style(string(body))
+	var s reqStyle
+	err = json.Unmarshal(body, &s)
+	check(err)
 
-	fmt.Fprintf(w, string("done"))
+	err = ioutil.WriteFile("./tmp/tmp-style."+s.UUID+".txt", []byte(get_style(s.Style)), 0644)
+	check(err)
+
+	fmt.Fprintf(w, "ok")
 }
 
 func format(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
 
 	body, err := ioutil.ReadAll(r.Body)
 	check(err)
 
-	err = ioutil.WriteFile("./tmp/tmp.txt", body, 0644)
+	var f reqFormat
+	err = json.Unmarshal(body, &f)
 	check(err)
 
-	s := get_style(format_style)
-	fmt.Println("current format-style is", s)
+	err = ioutil.WriteFile("./tmp/tmp-code."+f.UUID+"txt", f.Code, 0644)
+	check(err)
 
-	stdout, err := exec.Command("clang-format", s, "./tmp/tmp.txt").Output()
+	var style string
+	_, err = os.Stat("./tmp/tmp-style." + f.UUID + ".txt")
+	if err != nil {
+		style = "-style=LLVM"
+	} else {
+		s, err := ioutil.ReadFile("./tmp/tmp-style." + f.UUID + ".txt")
+		check(err)
+		style = string(s)
+		err = os.Remove("./tmp/tmp-style." + f.UUID + ".txt")
+		check(err)
+	}
+
+	fmt.Println("Request format-style is", style)
+
+	stdout, err := exec.Command("clang-format", style, "./tmp/tmp-code."+f.UUID+"txt").Output()
 	check(err)
 
 	enc := base64.StdEncoding.EncodeToString(stdout)
-
 	fmt.Fprintf(w, enc)
 
-	err = os.Remove("./tmp/tmp.txt")
+	err = os.Remove("./tmp/tmp-code." + f.UUID + "txt")
 	check(err)
 }
 
